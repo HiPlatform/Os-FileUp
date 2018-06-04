6e3a0579d1bb2f5f2bc47e9df3da5073addfb926
Sergio Henrique Pereira Reis <sergio.reis@hiplatform.com>
2018-06-04 11:10:38 -0300

    Mergin code to download attachment

--- app/src/main/java/com/inf/os/fileup/MainActivity.java
+++ app/src/main/java/com/inf/os/fileup/MainActivity.java
@@ -1,27 +1,33 @@
 package com.inf.os.fileup;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Build;
-import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.DownloadListener;
+import android.webkit.URLUtil;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 import java.io.File;
@@ -29,53 +35,25 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity{
-    WebView webView;
+
+public class MainActivity extends AppCompatActivity {
+    private Context mContext;
+    private Activity mActivity;
+
+    private LinearLayout mRootLayout;
+    private WebView mWebView;
+
     private static final String TAG = MainActivity.class.getSimpleName();
     private String mCM;
     private ValueCallback<Uri> mUM;
     private ValueCallback<Uri[]> mUMA;
     private final static int FCR=1;
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
-        super.onActivityResult(requestCode, resultCode, intent);
-        if(Build.VERSION.SDK_INT >= 21){
-            Uri[] results = null;
-            //Check if response is positive
-            if(resultCode== Activity.RESULT_OK){
-                if(requestCode == FCR){
-                    if(null == mUMA){
-                        return;
-                    }
-                    if(intent == null || intent.getData() == null){
-                        //Capture Photo if no image available
-                        if(mCM != null){
-                            results = new Uri[]{Uri.parse(mCM)};
-                        }
-                    }else{
-                        String dataString = intent.getDataString();
-                        if(dataString != null){
-                            results = new Uri[]{Uri.parse(dataString)};
-                        }
-                    }
-                }
-            }
-            mUMA.onReceiveValue(results);
-            mUMA = null;
-        }else{
-            if(requestCode == FCR){
-                if(null == mUM) return;
-                Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
-                mUM.onReceiveValue(result);
-                mUM = null;
-            }
-        }
-    }
+    private static final int MY_PERMISSION_REQUEST_CODE = 123;
 
     @SuppressLint({"SetJavaScriptEnabled", "WrongViewCast"})
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -83,24 +61,50 @@ public class MainActivity extends AppCompatActivity{
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 1);
         }
 
-        webView = (WebView) findViewById(R.id.ifView);
-        assert webView != null;
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setAllowFileAccess(true);
+        // Get the application context
+        mContext = getApplicationContext();
+        mActivity = MainActivity.this;
+
+        // Get the widget reference from xml layout
+        mRootLayout = (LinearLayout) findViewById(R.id.root_layout);
+        mWebView = (WebView) findViewById(R.id.web_view);
+        assert mWebView != null;
+
+        // Enable Debugging
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            mWebView.setWebContentsDebuggingEnabled(true);
+        }
+
+        // Enable localStorage
+        mWebView.getSettings().setDomStorageEnabled(true);
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setAllowFileAccess(true);
+
+        // Check permission for write external storage
+        checkPermission();
+
+        // The target url to surf using web view
+        String url = "https://testedt03.qa.directtalk.com.br/chat31/?idd=5925002486DA30054507";
+
+        // Load the url in web view
+        mWebView.loadUrl(url);
+
+        // Enable java script on web view
+        mWebView.getSettings().setJavaScriptEnabled(true);
 
         if(Build.VERSION.SDK_INT >= 21){
-            webSettings.setMixedContentMode(0);
-            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+            mWebView.getSettings().setMixedContentMode(0);
+            mWebView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
         }else if(Build.VERSION.SDK_INT >= 19){
-            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+            mWebView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
         }else if(Build.VERSION.SDK_INT < 19){
-            webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+            mWebView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
         }
-        webView.setWebViewClient(new Callback());
-        webView.loadUrl("https://infeeds.com/");
-        webView.getSettings().setDomStorageEnabled(true);
-        webView.setWebChromeClient(new WebChromeClient(){
+
+        mWebView.setWebViewClient(new Callback());
+
+        mWebView.setWebChromeClient(new WebChromeClient(){
             //For Android 3.0+
             public void openFileChooser(ValueCallback<Uri> uploadMsg){
                 mUM = uploadMsg;
@@ -169,7 +173,204 @@ public class MainActivity extends AppCompatActivity{
                 return true;
             }
         });
+
+        mWebView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDescription,
+                                        String mimetype, long contentLength) {
+                /*
+                    DownloadManager.Request
+                        This class contains all the information necessary to request a new download.
+                        The URI is the only required parameter. Note that the default download
+                        destination is a shared volume where the system might delete your file
+                        if it needs to reclaim space for system use. If this is a problem,
+                        use a location on external storage (see setDestinationUri(Uri).
+                */
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+
+                /*
+                    void allowScanningByMediaScanner ()
+                        If the file to be downloaded is to be scanned by MediaScanner, this method
+                        should be called before enqueue(Request) is called.
+                */
+                request.allowScanningByMediaScanner();
+
+                /*
+                    DownloadManager.Request setNotificationVisibility (int visibility)
+                        Control whether a system notification is posted by the download manager
+                        while this download is running or when it is completed. If enabled, the
+                        download manager posts notifications about downloads through the system
+                        NotificationManager. By default, a notification is shown only
+                        when the download is in progress.
+
+                        It can take the following values: VISIBILITY_HIDDEN, VISIBILITY_VISIBLE,
+                        VISIBILITY_VISIBLE_NOTIFY_COMPLETED.
+
+                        If set to VISIBILITY_HIDDEN, this requires the permission
+                        android.permission.DOWNLOAD_WITHOUT_NOTIFICATION.
+
+                    Parameters
+                        visibility int : the visibility setting value
+                    Returns
+                        DownloadManager.Request this object
+                */
+                request.setNotificationVisibility(
+                        DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+
+                /*
+                    DownloadManager
+                        The download manager is a system service that handles long-running HTTP
+                        downloads. Clients may request that a URI be downloaded to a particular
+                        destination file. The download manager will conduct the download in the
+                        background, taking care of HTTP interactions and retrying downloads
+                        after failures or across connectivity changes and system reboots.
+                */
+
+                /*
+                    String guessFileName (String url, String contentDisposition, String mimeType)
+                        Guesses canonical filename that a download would have, using the URL
+                        and contentDisposition. File extension, if not defined,
+                        is added based on the mimetype
+
+                    Parameters
+                        url String : Url to the content
+                        contentDisposition String : Content-Disposition HTTP header or null
+                        mimeType String : Mime-type of the content or null
+
+                    Returns
+                        String : suggested filename
+                */
+                String fileName = URLUtil.guessFileName(url,contentDescription,mimetype);
+
+                /*
+                    DownloadManager.Request setDestinationInExternalPublicDir
+                    (String dirType, String subPath)
+
+                        Set the local destination for the downloaded file to a path within
+                        the public external storage directory (as returned by
+                        getExternalStoragePublicDirectory(String)).
+
+                        The downloaded file is not scanned by MediaScanner. But it can be made
+                        scannable by calling allowScanningByMediaScanner().
+
+                    Parameters
+                        dirType String : the directory type to pass to
+                                         getExternalStoragePublicDirectory(String)
+                        subPath String : the path within the external directory, including
+                                         the destination filename
+
+                    Returns
+                        DownloadManager.Request this object
+
+                    Throws
+                        IllegalStateException : If the external storage directory cannot be
+                                                found or created.
+                */
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,fileName);
+
+                DownloadManager dManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+
+                /*
+                    long enqueue (DownloadManager.Request request)
+                        Enqueue a new download. The download will start automatically once the
+                        download manager is ready to execute it and connectivity is available.
+
+                    Parameters
+                        request DownloadManager.Request : the parameters specifying this download
+
+                    Returns
+                        long : an ID for the download, unique across the system. This ID is used
+                               to make future calls related to this download.
+                */
+                dManager.enqueue(request);
+            }
+        });
     }
+
+    protected void checkPermission(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+                if(shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+                    // show an alert dialog
+                    AlertDialog.Builder builder = new AlertDialog.Builder(mActivity);
+                    builder.setMessage("Write external storage permission is required.");
+                    builder.setTitle("Please grant permission");
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ActivityCompat.requestPermissions(
+                                    mActivity,
+                                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                    MY_PERMISSION_REQUEST_CODE
+                            );
+                        }
+                    });
+                    builder.setNeutralButton("Cancel",null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }else {
+                    // Request permission
+                    ActivityCompat.requestPermissions(
+                            mActivity,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            MY_PERMISSION_REQUEST_CODE
+                    );
+                }
+            }else {
+                // Permission already granted
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        switch(requestCode){
+            case MY_PERMISSION_REQUEST_CODE:{
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    // Permission granted
+                }else {
+                    // Permission denied
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+        super.onActivityResult(requestCode, resultCode, intent);
+        if(Build.VERSION.SDK_INT >= 21){
+            Uri[] results = null;
+            //Check if response is positive
+            if(resultCode== Activity.RESULT_OK){
+                if(requestCode == FCR){
+                    if(null == mUMA){
+                        return;
+                    }
+                    if(intent == null || intent.getData() == null){
+                        //Capture Photo if no image available
+                        if(mCM != null){
+                            results = new Uri[]{Uri.parse(mCM)};
+                        }
+                    }else{
+                        String dataString = intent.getDataString();
+                        if(dataString != null){
+                            results = new Uri[]{Uri.parse(dataString)};
+                        }
+                    }
+                }
+            }
+            mUMA.onReceiveValue(results);
+            mUMA = null;
+        }else{
+            if(requestCode == FCR){
+                if(null == mUM) return;
+                Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+                mUM.onReceiveValue(result);
+                mUM = null;
+            }
+        }
+    }
+
     public class Callback extends WebViewClient{
         public void onReceivedError(WebView view, int errorCode, String description, String failingUrl){
             Toast.makeText(getApplicationContext(), "Failed loading app!", Toast.LENGTH_SHORT).show();
@@ -187,8 +388,8 @@ public class MainActivity extends AppCompatActivity{
         if(event.getAction() == KeyEvent.ACTION_DOWN){
             switch(keyCode){
                 case KeyEvent.KEYCODE_BACK:
-                    if(webView.canGoBack()){
-                        webView.goBack();
+                    if(mWebView.canGoBack()){
+                        mWebView.goBack();
                     }else{
                         finish();
                     }
